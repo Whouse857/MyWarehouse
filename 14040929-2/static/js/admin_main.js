a37c3a0f0e1ee60021b295be03def26173788ace
Whouse857 <hamedsargoli857@gmail.com>
2025-12-22 21:32:34 +0330

    clear repositor

--- 14040929-2/static/js/admin_main.js
+++
@@ -1,7 +0,0 @@
-// [TAG: ADMIN_MAIN]
-// نقطه ورود بخش مدیریت - جایگزین نهایی Admin Pages.js
-
-// این فایل فقط وظیفه هماهنگی و اطمینان از بارگذاری ماژول‌ها را دارد.
-// تمام توابع در فایل‌های admin_users.js، admin_management.js و admin_backup.js تعریف شده‌اند.
-
-console.log("Admin Modules Loaded Successfully.");
